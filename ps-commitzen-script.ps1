6545649604be0a630b117de51e6a3364ed40e7d5
Lucas Felix <Lucas@glide.com.br>
2024-11-04 17:22:51 -0300

    feat: implement package installation checks

--- ps-commitzen-script.ps1
+++ ps-commitzen-script.ps1
@@ -1,28 +1,55 @@
 # Define a política de execução, suprimindo a saída
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force -ErrorAction SilentlyContinue
 
-# Instala o Scoop
-Write-Host "Instalando o Scoop..."
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+# Função para verificar se um pacote está instalado
+function Is-PackageInstalled {
+    param (
+        [string]$packageName
+    )
+    return (scoop list | Select-String $packageName) -ne $null
+}
 
-# Instala o Python
-scoop install python
+# Instala o Scoop se não estiver instalado
+if (-not (Is-PackageInstalled "scoop")) {
+    Write-Host "`nInstalando o Scoop..." -ForegroundColor Green
+    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+} else {
+    Write-Host "`nScoop já está instalado." -ForegroundColor Green
+}
 
-# Instala o pipx
-Write-Host "Instalando o pipx..."
-scoop install pipx
+# Instala o Python se não estiver instalado
+if (-not (Is-PackageInstalled "python")) {
+    Write-Host "`nInstalando o Python..." -ForegroundColor Green
+    scoop install python
+} else {
+    Write-Host "`nPython já está instalado." -ForegroundColor Green
+}
+
+# Instala o pipx se não estiver instalado
+if (-not (Is-PackageInstalled "pipx")) {
+    Write-Host "`nInstalando o pipx..." -ForegroundColor Green
+    scoop install pipx
+} else {
+    Write-Host "`npipx já está instalado." -ForegroundColor Green
+}
 
 # Garante que o pipx esteja no PATH
-Write-Host "Configurando o pipx no PATH..."
+Write-Host "`nConfigurando o pipx no PATH..." -ForegroundColor Green
 pipx ensurepath
 
-# Instala o commitizen
-Write-Host "Instalando o commitizen..."
-pipx install commitizen
+# Instala o commitizen se não estiver instalado
+if (-not (pipx list | Select-String "commitizen")) {
+    Write-Host "`nInstalando o commitizen..." -ForegroundColor Green
+    pipx install commitizen
+} else {
+    Write-Host "`ncommitizen já está instalado." -ForegroundColor Green
+}
+
+# Atualiza o commitizen
+Write-Host "`nAtualizando o commitizen..." -ForegroundColor Green
 pipx upgrade commitizen
 
-Write-Host "Instalação concluída!"
+Write-Host "`nInstalacao concluida!" -ForegroundColor Green
 
 # Espera por uma entrada do usuário antes de fechar
 Read-Host "Pressione Enter para sair..."
